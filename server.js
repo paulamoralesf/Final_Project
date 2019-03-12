0332dc305f207863e69813d64b1736a1023294be
Paula Morales <paulaa.moralesf@gmail.com>
2019-03-12 18:58:21 -0500

    FinalProject sí

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser=require('body-parser')
 const {Medicos}=require('./models//index')
 const {Pacientes}=require('./models//index')
 const {Farmacos}=require('./models//index')
-const PORT=3000 || process.env.PORT
+const PORT=process.env.PORT || 3000 
 const app=express();
 
 
